322b183ada5977dc254d00acefc60b6311d8ea8c
Mateusrb6 <148596676+Mateusrb6@users.noreply.github.com>
2024-03-28 15:54:28 -0300

    Update ex2.cpp

--- fundamentos/ex2.cpp
+++ fundamentos/ex2.cpp
@@ -5,15 +5,15 @@ int num1, num2, soma, diferenca, quociente;
 int main()
 {
 
-    cout << "forneça dois numeros \n";
+    cout << "forneça dois números \n";
     cin >> num1 >> num2;
 
     soma = num1 + num2;
     diferenca = num1 - num2;
     quociente = num1 / num2;
 
-    cout << "a soma é igual a" << soma << endl; 
-    cout << "a diferenca é igual a " << diferenca << endl;
+    cout << "a soma é igual a " << soma << endl; 
+    cout << "a diferença é igual a " << diferenca << endl;
     cout << "o quociente é igual a " << quociente << endl;
 
         return 0;
