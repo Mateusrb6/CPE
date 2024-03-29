7d94d94e770137e3dc3e0841a192140e6349a075
Mateusrb6 <148596676+Mateusrb6@users.noreply.github.com>
2024-03-29 01:37:17 -0300

    Update ex4.cpp

--- fundamentos/ex4.cpp
+++ fundamentos/ex4.cpp
@@ -3,7 +3,60 @@ using namespace std;
 
 int main(){
 
-cout
-    
+cout << "Quadrado" << endl;
+
+for (int i = 0; i < 9; i++)
+{
+  cout << "*";
+}
+
+cout << "\n";
+
+for (int i = 0; i < 8; i++)
+{
+  cout << "*       * \n";
+}
+
+for (int i = 0; i < 9; i++)
+{
+  cout << "*";
+}
+
+cout << "\n";
+
+cout << "Circulo oval" << endl;
+
+cout << "   ***   \n";
+cout << " *     * \n";
+
+for (int i = 0; i < 4; i++)
+{
+  cout << "*       * \n";
+}
+
+cout << " *     * \n";
+cout << "   ***   \n";
+
+cout << "Seta" << endl;
+
+cout << "  *  " << endl;
+cout << " *** " << endl;
+cout << "*****" << endl;
+
+for (int i = 0; i < 6; i++)
+{
+  cout << "  *  \n";
+}
+
+cout << "losango" << endl;
+
+cout << "    *     \n";
+cout << "   * *     \n";
+cout << "  *   *     \n";
+cout << " *     *     \n";
+cout << "  *   *     \n";
+cout << "   * *     \n";
+cout << "    *     \n";
+
   return 0;
 }
