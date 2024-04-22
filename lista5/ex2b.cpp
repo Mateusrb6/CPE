0f6e46bde1ac4ad95dfed6d33177f3d2e0c84b9f
Mateusrb6 <148596676+Mateusrb6@users.noreply.github.com>
2024-04-22 15:31:44 -0300

    Update ex2b.cpp

--- lista5/ex2b.cpp
+++ lista5/ex2b.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+
+bool isPrime(int num) {
+    if (num <= 1)
+        return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    std::cout << "Números primos entre 2 e 1000:" << std::endl;
+    for (int i = 2; i <= 1000; i++) {
+        if (isPrime(i))
+            std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
 
